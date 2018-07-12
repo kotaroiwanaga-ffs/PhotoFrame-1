e19c88a553e3542a9b53459e532c9568c6c630dd
直喜 永石 <naoki.nagaishi@fujifilm.com>
2018-07-12 11:29:53 +0900

    07/12午前終了

--- PhotoFrameApp/SlideShow.cs
+++ PhotoFrameApp/SlideShow.cs
@@ -13,14 +13,51 @@ namespace PhotoFrameApp
 {
     public partial class SlideShow : Form
     {
+        private IEnumerable<Photo> photolist_listview;//リストビュー上のフォト
+        private IEnumerable<Album> albumlist;
+
         public SlideShow(IEnumerable<Photo> photolist)
         {
+            this.photolist_listview = photolist;
+            //this.albumlist = GetAllAlbums();
             InitializeComponent();
+
+
+
+
         }
 
         private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
         {
 
+        }
+
+      
+
+        private void radioButton_AlbumSlideShow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_AlbumSlideShow.Checked == true)
+            {
+                comboBox_AlbumName.Enabled = true;
+                comboBox_AlbumName.Items.Add();
+            }
+            else
+            {
+                comboBox_AlbumName.Enabled = false;
+            }
+        }
+
+        private void radioButton_ListViewSlideShow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_AlbumSlideShow.Checked == true)
+            {
+                comboBox_AlbumName.Enabled = true;
+
+            }
+            else
+            {
+                comboBox_AlbumName.Enabled = false;
+            }
         }
     }
 }
