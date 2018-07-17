c7657ee143b5e7655b4372e25915f72980c97c00
kotaroiwanaga-ffs <38637016+kotaroiwanaga-ffs@users.noreply.github.com>
2018-07-17 16:49:06 +0900

    vsまじくそやで

---
+++ PhotoFrame.ApplicationTests1/PhotoFrameApplicationTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoFrame.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoFrame.Application.Tests
+{
+    [TestClass()]
+    public class PhotoFrameApplicationTests
+    {
+
+        [TestMethod()]
+        public void PhotoFrameApplicationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchFolderTest()
+        {
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FilterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddKeywordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteKeywordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToggleIsFavoriteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddAlbumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchAlbumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SortDateAscendingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SortDateDescendingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllAlbumsTest()
+        {
+            Assert.Fail();
+        }
+
+    
+    }
+}
