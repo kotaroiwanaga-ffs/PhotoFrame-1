c62200d32b084009ebd9d2718865010f05fee67b
直喜 永石 <naoki.nagaishi@fujifilm.com>
2018-07-18 16:40:29 +0900

    0718授業終了

--- PhotoFrameApp/SlideShow.cs
+++ PhotoFrameApp/SlideShow.cs
@@ -324,8 +324,6 @@ namespace PhotoFrameApp
             comboBox_AlbumName.Enabled = true;
             textBox_SaveAlbumName.Enabled = true;
             button_SaveAlbumName.Enabled = true;
-            button_Next.Enabled = true;
-            button_Back.Enabled = true;
 
             button_Back.Enabled = false;
             button_Next.Enabled = true;
@@ -352,6 +350,7 @@ namespace PhotoFrameApp
                 button_Next.Enabled = true;
                 button_Pause_SlideShow.Enabled = false;
                 button_StopSlideShow.Enabled = false;
+                button_StartSlideShow.Enabled = true;
             }
             else if (slideindex == this.slideshow_list.Count() - 1)
             {
@@ -369,7 +368,7 @@ namespace PhotoFrameApp
 
                 button_Pause_SlideShow.Enabled = false;
                 button_StopSlideShow.Enabled = true;
-
+                button_StartSlideShow.Enabled = true;
 
             }
 
