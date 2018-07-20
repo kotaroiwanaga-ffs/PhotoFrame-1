8a51b66d45f0f76c27522feb23e6bd82850dbf7c
kotaroiwanaga-ffs <38637016+kotaroiwanaga-ffs@users.noreply.github.com>
2018-07-20 14:34:42 +0900

    テストプログラム追加(ビジネスロジック)

--- PhotoFrame.Application/PhotoFrameApplication.cs
+++ PhotoFrame.Application/PhotoFrameApplication.cs
@@ -103,62 +103,6 @@ namespace PhotoFrame.Application
             return this.getAllAlbums.Execute();
         }
 
-        //public int CreateAlbum(string albumName)
-        //{
-        //    return createAlbum.Execute(albumName);
-        //}
-
-        //public async Task<int> CreateAlbumAsync(string albumName)
-        //{
-        //    return await createAlbum.ExecuteAsync(albumName);
-        //}
-
-
-
-        //public IEnumerable<Photo> SearchAlbum(string albumName)
-        //{
-        //    return searchAlbum.Execute(albumName);
-        //}
-
-        //public async Task<IEnumerable<Photo>> SearchAlbumAsync(string albumName)
-        //{
-        //    return await searchAlbum.ExecuteAsync(albumName);
-        //}
-
-
-
-        //public IEnumerable<Photo> SearchDirectory(string directoryName)
-        //{
-        //    return searchDirectory.Execute(directoryName);
-        //}
-
-        //public async Task<IEnumerable<Photo>> SearchDirectoryAsync(string directoryName)
-        //{
-        //    return await searchDirectory.ExecuteAsync(directoryName);
-        //}
-
-
-
-        //public Photo ToggleFavorite(Photo photo)
-        //{
-        //    return toggleFavorite.Execute(photo);
-        //}
-
-        //public async Task<Photo> ToggleFavoriteAsync(Photo photo)
-        //{
-        //    return await toggleFavorite.ExecuteAsync(photo);
-        //}
-
-
-
-        //public Photo ChangeAlbum(Photo photo, string newAlbumName)
-        //{
-        //    return changeAlbum.Execute(photo, newAlbumName);
-        //}
-
-        //public async Task<Photo> ChangeAlbumAsync(Photo photo, string newAlbumName)
-        //{
-        //    return await changeAlbum.ExecuteAsync(photo, newAlbumName);
-        //}
+        
     }
 }
